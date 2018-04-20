78cf3eed879916690a9fa98389f4811253065070
Dylan <dylan.ebert@gmail.com>
2018-04-20 16:20:19 -0400

    ..

---
+++ reset_rig.py
@@ -0,0 +1,8 @@
+import maya.cmds as cmds
+
+cmds.setAttr('foot_controller_L.translateX', 0)
+cmds.setAttr('foot_controller_L.translateY', 0)
+cmds.setAttr('foot_controller_L.translateZ', 0)
+cmds.setAttr('foot_controller_L.rotateX', 0)
+cmds.setAttr('foot_controller_L.rotateY', 0)
+cmds.setAttr('foot_controller_L.rotateX', 0)
